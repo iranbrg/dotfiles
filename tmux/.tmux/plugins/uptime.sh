c34bb081e7dca1c479a1346489a8927d1ee27d31
Iran Braga <iran.brg@gmail.com>
2020-12-13 20:57:31 -0300

    Script to set formated uptime for Tmux added

---
+++ tmux/.tmux/plugins/uptime.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# Script to display formated uptime in tmux status bar
+# It was necessary because escaping characters in tmux is a pain in the ass
+
+# Format: DD:HH:MM:SS
+# awk '{printf("%d:%02d:%02d:%02d\n",($1/60/60/24),($1/60/60%24),($1/60%60),($1%60))}' /proc/uptime
+
+# Format: XXh XXm
+awk '{printf("%02dh %02dm\n",($1/60/60%24),($1/60%60))}' /proc/uptime
